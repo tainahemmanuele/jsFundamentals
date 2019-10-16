266b9fd2feae2fa99089d4ef717a8470a44b16cc
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-10-16 13:46:56 -0300

    Curso concluido

--- webpack-starter/src/scripts/index.js
+++ webpack-starter/src/scripts/index.js
@@ -1,16 +1,3 @@
-let form = document.getElementById('user-form');
-form.addEventListener('submit', event =>{
-    let user = form.elements['user'];
-    let userError = document.getElementById('user-error');
+let inputString = "alert('Hello');";
 
-    if(user.value.length <= 4){
-    userError.textContent = 'Invalid entry';
-    userError.style.color = 'red';
-    user.style.borderColor = 'red';
-    user.focus();
-
-    event.preventDefault();
-    }
-
-
-});
+eval(inputString);
